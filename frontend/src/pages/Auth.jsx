b018369454d8e0c883eedb29aca4b9ae55b8182e
Kavini Wickramasooriya <iresheranga2020al@gmail.com>
2026-04-23 02:07:33 +0530

    update

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -11,7 +11,7 @@ import 'sweetalert2/dist/sweetalert2.min.css';
 
 import logo from '../assets/logo.png';
 
-const API_BASE = import.meta.env.VITE_AUTH_API || 'http://localhost:3001/api';
+const API_BASE = import.meta.env.VITE_AUTH_API;
 
 function Auth() {
   const [isLogin, setIsLogin] = useState(true);
